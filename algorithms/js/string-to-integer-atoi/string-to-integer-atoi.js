3e00def4a87d841db50b35fc217700deda40c1d0
gitadmin <525151673@qq.com>
2019-05-14 18:14:20 +0800

    add 7-9

---
+++ algorithms/js/string-to-integer-atoi/string-to-integer-atoi.js
@@ -0,0 +1,61 @@
+/**********************************************************************************
+ * 8. 字符串转换整数 (atoi)
+ * 
+ * 请你来实现一个 atoi 函数，使其能将字符串转换成整数
+ * 首先，该函数会根据需要丢弃无用的开头空格字符，直到寻找到第一个非空格的字符为止
+ *
+ * 当我们寻找到的第一个非空字符为正或者负号时，则将该符号与之后面尽可能多的连续数字组合起来，作为该整数的正负号；
+ * 假如第一个非空字符是数字，则直接将其与之后连续的数字字符组合起来，形成整数。
+ * 
+ * 该字符串除了有效的整数部分之后也可能会存在多余的字符，这些字符可以被忽略，它们对于函数不应该造成影响。
+ * 
+ * 注意：假如该字符串中的第一个非空格字符不是一个有效整数字符、字符串为空或字符串仅包含空白字符时，则你的函数不需要进行转换。
+ * 
+ * 在任何情况下，若函数不能进行有效的转换时，请返回 0。
+ * 
+ * 说明：
+ * 假设我们的环境只能存储 32 位大小的有符号整数，那么其数值范围为 [−231,  231 − 1]。如果数值超过这个范围，qing返回  INT_MAX (231 − 1) 或 INT_MIN (−231) 
+ * 
+ **********************************************************************************/
+
+ /**
+ * @param {string} str
+ * @return {number}
+ */
+var myAtoi = function(str) {
+    if(str.length == 0) return 0
+       
+       let result = '', lastChar = '',findfirst = false
+   
+       let nums = ['0','1','2','3','4','5','6','7','8','9']
+   
+       let syms = ['+','-']
+   
+       for(let i = 0; i < str.length; i++){
+        
+           let char = str[i]
+   
+           if(char == ' ' && !findfirst) continue
+   
+           if(syms.indexOf(lastChar) != -1 && nums.indexOf(char) == -1) return 0
+   
+           if(findfirst && nums.indexOf(char) == -1) break
+   
+           if(nums.indexOf(char) != -1 || syms.indexOf(char) != -1){
+   
+               findfirst = true
+   
+               result = result + char
+   
+           } else{
+   
+               return 0
+           }
+   
+           lastChar = char
+       }
+   
+       if(result.length == 0 || syms.indexOf(result) != -1) return 0
+   
+       return Math.min(2147483647,Math.max(parseInt(result),-2147483648))
+   };
